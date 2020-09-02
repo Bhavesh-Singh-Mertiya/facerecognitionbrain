15ff2e5c7f53ada64bd4586c9c5236adceeb90a1
Bhavesh-Singh-Mertiya <26bhaveshsingh@gmail.com>
2020-09-02 12:00:50 +0530

    Updates

---
+++ precache-manifest.6d2d1b3c59663081337403e704dc46d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "702d0299b7c47e60907a",
+    "url": "/facerecognitionbrain/static/css/main.c262136e.chunk.css"
+  },
+  {
+    "revision": "702d0299b7c47e60907a",
+    "url": "/facerecognitionbrain/static/js/main.760f8017.chunk.js"
+  },
+  {
+    "revision": "ce2d7a8efc8c8d84e679",
+    "url": "/facerecognitionbrain/static/js/runtime~main.2d683c34.js"
+  },
+  {
+    "revision": "ccccc56ecd28fcaf4caf",
+    "url": "/facerecognitionbrain/static/css/2.2f09decc.chunk.css"
+  },
+  {
+    "revision": "ccccc56ecd28fcaf4caf",
+    "url": "/facerecognitionbrain/static/js/2.f2693ece.chunk.js"
+  },
+  {
+    "revision": "4ff6394696c6656808573a71aa039373",
+    "url": "/facerecognitionbrain/index.html"
+  }
+];
